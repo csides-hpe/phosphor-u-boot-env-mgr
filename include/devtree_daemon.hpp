e666485339792e5b3ba3de8b8b7a66fd37853362
Chris Sides <christopher.sides@hpe.com>
2024-05-08 10:43:23 -0500

    This should be enough to build the new and old daemons with meson

---
+++ include/devtree_daemon.hpp
@@ -0,0 +1,59 @@
+/*
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+
+#include <boost/asio/io_service.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+static constexpr const char* DevTreeDaemonServiceName =
+    "xyz.openbmc_project.U_Boot.Environment.DevTreeDaemon";
+static constexpr const char* DevTreeDaemonIface =
+    "xyz.openbmc_project.U_Boot.Environment.DaemonManager";
+static constexpr const char* DevTreeDaemonPath =
+    "/xyz/openbmc_project/u_boot/environment/dtdaemon";
+
+static constexpr const char* DevTreeVPDIface = 
+    "xyz.openbmc_project.MachineContext";
+static constexpr const char* DevTreeVPDPath = 
+    "/xyz/openbmc_project/machinecontext";
+
+
+//maybe have .first be the full path and another field for the property name to display as? perhaps as a path?
+/*
+static constexpr const std::vector<
+                        std::pair<std::string, std::string>> nodeInputs = { {"model", "model"}, 
+                                                                            {"local-mac-address", "mac1"}, 
+                                                                            {"mac-address", "mac2"}, 
+                                                                            {"serial-number", "serial-number"}
+                                                                      }
+  */
+  
+class DevTreeDaemon
+{
+    boost::asio::io_service& io;
+    sdbusplus::asio::object_server& server;
+    std::shared_ptr<sdbusplus::asio::connection> conn;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface;
+
+    //Note: may want to create a function for refreshing data from device-tree (but probably reaaaally don't need it for now, so skip it and 
+    //consider it for later on)
+
+  public:
+    DevTreeDaemon(boost::asio::io_service& io,
+                sdbusplus::asio::object_server& srv,
+                std::shared_ptr<sdbusplus::asio::connection>& conn);
+};
---
+++ src/devtree_daemon.cpp
@@ -0,0 +1,85 @@
+/*
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "devtree_daemon.hpp"
+#include <phosphor-logging/log.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <boost/process/child.hpp>
+#include <boost/process/io.hpp>
+#include <vector>
+#include <fstream>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+//template <typename... ArgTypes>
+//think this (template above) applied only to an 'execcuse command' function
+
+DevTreeDaemon::DevTreeDaemon(boost::asio::io_service& io_,
+                         sdbusplus::asio::object_server& srv_,
+                         std::shared_ptr<sdbusplus::asio::connection>& conn_) :
+    io(io_),
+    server(srv_), conn(conn_)
+{
+    iface = server.add_interface(DevTreeDaemonPath, DevTreeDaemonIface);
+
+    std::string nodeBasePath= "/sys/";
+    std::string nodeFullPath; //will hold an absolute path to the node
+    std::string nodeHandle; //TODO: for filename alone (strip out everything except whatever is inside a pair of '/' marks, explicit or implied)
+    
+    std::vector<
+      std::pair<std::string, std::string>> nodeInputs = { {"model", "model"}, 
+                                                                            {"local-mac-address", "mac1"}, 
+                                                                            {"mac-address", "mac2"}, 
+                                                                            {"serial-number", "serial-number"}
+                                                                        };
+
+    //iterate through all supported nodes
+    for (std::pair<std::string, std::string> nodeData : nodeInputs) //so we can modify the contents of nodeData
+    {
+        std::string nodeRelativePath= nodeData.first;
+        std::string nodeValue;
+        nodeFullPath= nodeBasePath + nodeRelativePath;
+
+        //TODO: strip any '/' character from the end of the string, then strip everything up to the 'last' / in the string
+        //this code will work fine as-is as long as only 'simple' node paths are used
+        nodeHandle = nodeRelativePath; //temporary, replace this later with something that actually looks at the string and strips things out (for more complex paths)
+
+        std::ifstream fruStream;
+        fruStream.open(nodeFullPath);
+        if (!fruStream || !std::getline(fruStream, nodeValue))
+            continue;
+
+        std::string dBuspropertyName = nodeData.second;
+        iface->register_property(dBuspropertyName, nodeValue);
+
+        //if file exists, then read string @ first, then write data to @ secon
+    }
+
+    iface->initialize(true);
+}
+
+int main()
+{ 
+    boost::asio::io_service io;
+    auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+    conn->request_name(DevTreeDaemonServiceName);
+    sdbusplus::asio::object_server server(conn);
+
+    DevTreeDaemon devTreeDaemon(io, server, conn);
+
+    io.run();
+
+    return 0;
+}
