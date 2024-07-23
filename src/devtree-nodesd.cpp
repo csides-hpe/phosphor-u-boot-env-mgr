7f44e72c51c58d18018ac4474cd398673a9da477
Christopher Sides <christopher.sides@hp.com>
2024-07-23 07:51:07 -0700

    Confirmed dbus service works w/ api update

--- src/devtree-nodesd.cpp
+++ src/devtree-nodesd.cpp
@@ -1,76 +1,87 @@
-/*
-// Copyright (c) 2024 Hewlett Packard Enterprise
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-*/
+#include <xyz/openbmc_project/MachineContext/server.hpp>
+#include <sdbusplus/server.hpp>
 
-#include "devtree-nodesd.hpp"
-
-#include <boost/process/child.hpp>
-#include <boost/process/io.hpp>
-#include <phosphor-logging/elog-errors.hpp>
-#include <phosphor-logging/log.hpp>
-#include <xyz/openbmc_project/Common/error.hpp>
-
+#include <iostream>
 #include <fstream>
 #include <vector>
+#include <string_view>
 
-DevTreeNodesDaemon::DevTreeNodesDaemon(
-    boost::asio::io_service& io_, sdbusplus::asio::object_server& srv_,
-    std::shared_ptr<sdbusplus::asio::connection>& conn_) :
-    io(io_),
-    server(srv_), conn(conn_)
+using MachineContext_inherit =
+    sdbusplus::server::object_t<sdbusplus::server::xyz::openbmc_project::MachineContext>;
+
+/** Example implementation of net.poettering.Calculator */
+struct MachineContext : MachineContext_inherit
 {
-    //Check if these device-tree nodes are present and populated
-    //supportedNodes pair: <node name, dbus property (display) name>
-    std::vector<std::pair<std::string, std::string>> supportedNodes = {
-        {"model", "model"},
-        {"local-mac-address", "mac1"},
-        {"mac-address", "mac2"},
-        {"serial-number", "serial-number"}};
-
-    iface = server.add_interface(MachineContextPath, MachineContextIface);
-
-    // iterate over supported nodes
-    for (std::pair<std::string, std::string> nodeData : supportedNodes)
+    /** Constructor */
+    MachineContext(sdbusplus::bus_t& bus, const char* path) :
+        MachineContext_inherit(bus, path)
     {
-        std::string nodeRelativePath = nodeData.first;
-        std::string nodeFullPath = nodeBasePath + nodeRelativePath;
-        std::string nodeValue;
 
-        std::ifstream fruStream;
-        fruStream.open(nodeFullPath);
-        if (!fruStream || !std::getline(fruStream, nodeValue))
-            continue;
+        std::vector<std::pair<std::string, std::string>> supportedNodes = {
+            {"model", "model"},
+            {"local-mac-address", "mac1"},
+            {"mac-address", "mac2"},
+            {"serial-number", "serial-number"}};
 
-        // dtree node was sucessfully read, write the data to dbus under given property name
-        std::string dBuspropertyName = nodeData.second;
-        iface->register_property(dBuspropertyName, nodeValue);
+        static constexpr const char* nodeBasePath = "/proc/device-tree/";
+
+        MachineContext::model("N/A");
+        MachineContext::serialnumber("N/A");
+        MachineContext::maCaddress("N/A");
+        MachineContext::localMACaddress("N/A");
+
+        for (std::pair<std::string, std::string> nodeData : supportedNodes)
+        {
+            std::string nodeRelativePath = nodeData.first;
+            std::string nodeFullPath = nodeBasePath + nodeRelativePath;
+            std::string nodeValue;
+
+            std::ifstream fruStream;
+            fruStream.open(nodeFullPath);
+            if (!fruStream || !std::getline(fruStream, nodeValue))
+                continue;
+
+            if (nodeData.first == "model")
+            {
+                    MachineContext::model(nodeValue);
+            }
+            else if (nodeData.first == "serial-number")
+            {
+                MachineContext::serialnumber(nodeValue);
+            }
+            else if (nodeData.first == "mac-address")
+            {
+                MachineContext::maCaddress(nodeValue);
+            }
+            else if (nodeData.first == "serial-number")
+            {
+                MachineContext::localMACaddress(nodeValue);
+            }
+
+            // dtree node was sucessfully read, write the data to dbus under given
+            // property name
+            //std::string dBuspropertyName = nodeData.second;
+            //iface->register_property(dBuspropertyName, nodeValue);
+        }
+
+        
+        //MachineContext::serialnumber("testing"); 
     }
-
-    iface->initialize(true);
-}
+};
 
 int main()
 {
-    boost::asio::io_service io;
-    auto conn = std::make_shared<sdbusplus::asio::connection>(io);
-    conn->request_name(MachineContextServiceName);
-    sdbusplus::asio::object_server server(conn);
+    // Create a new bus and affix an object manager for the subtree path we
+    // intend to place objects at..
+    auto b = sdbusplus::bus::new_default();
+    sdbusplus::server::manager_t m{b, MachineContext::instance_path};
 
-    DevTreeNodesDaemon devTreeNodesDaemon(io, server, conn);
+    // Reserve the dbus service name : net.poettering.Calculator
+    b.request_name(MachineContext::default_service);
 
-    io.run();
+    // Create a calculator object at /net/poettering/calculator
+    MachineContext m1{b, MachineContext::instance_path};
 
-    return 0;
+    // Handle dbus processing forever.
+    b.process_loop();
 }
